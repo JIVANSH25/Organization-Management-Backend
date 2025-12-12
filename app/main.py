29111077820433925bf44ad08a3078c920ba8365
JIVANSH ANAND <anandjivansh@gmail.com>
2025-12-13 00:48:02 +0530

    Add initial code

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from mangum import Mangum
-from app.routers import org as org_router
-from app.routers import admin as admin_router
+
+from app.routers.org import router as org_router
+from app.routers.admin import router as admin_router
 
 app = FastAPI(title="Org Management Service")
 
-app.include_router(org_router.router)
-app.include_router(admin_router.router)
+app.include_router(org_router)
+app.include_router(admin_router)
 
 @app.get("/")
 async def root():
     return {"msg": "Org Management Backend running on Vercel"}
 
-# ADD THIS
 handler = Mangum(app)
--- app/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Routers Package Initialization
-Allows FastAPI to automatically recognize router modules.
-"""
-
-from .org import router as org_router
-from .admin import router as admin_router
-
-__all__ = ["org_router", "admin_router"]
